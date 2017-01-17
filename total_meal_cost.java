6f23c04ce708ffd9928e3d6ebdb7161cafe0f2fb
pranchalsharma <pranchalsharma@qainfotech.com>
2017-01-18 01:32:46 +0530

    Add files via upload

---
+++ total_meal_cost.java
@@ -0,0 +1,26 @@
+
+package mealcost;
+
+import java.util.Scanner;
+
+public class total_meal_cost {
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Enter cost of the meal");
+        double mealCost = scan.nextDouble(); // original meal price
+        System.out.println("Enter tip percentage");
+        int tipPercent = scan.nextInt(); // tip percentage
+        System.out.println("Enter tax percentage");
+        int taxPercent = scan.nextInt(); // tax percentage
+        
+        double tip = (mealCost *((double)tipPercent/100)); //calculating tip amount
+        double tax = (mealCost*((double)taxPercent/100)); //calculating tax amount
+        double total= (mealCost+tip+tax); //calculating total amount
+        
+        //int totalCost = (int) Math.round(total); //rounding off total meal cost
+
+        System.out.println("The total meal cost is "+total+" INR");
+	}
+}
